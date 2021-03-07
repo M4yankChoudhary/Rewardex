74a6a0542826360b104243fb16e725ac0b33e923
Mayank Choudhary <7mayankchoudhary7@gmail.com>
2021-03-07 13:03:13 +0530

    Update urls.py

--- merchants/urls.py
+++ merchants/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.merchant_dashboard, name="merchantDashboard"),
     path('merchant/login/', views.merchant_login, name="merchantLogin"),
     path('merchant/events/', views.merchant_events, name="merchantEvents"),
+    path('merchant/welcome/', views.merchant_welcome, name="merchantWelcome")
     path('merchant/spinform/', views.spin_event, name="merchantspin"),
     path('merchant/qrform', views.qrcode_event, name="merchantqr"),
     path('merchant/scrachform', views.scrach_event, name="merchantscrach")
