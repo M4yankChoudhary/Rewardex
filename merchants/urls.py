e968476cbdc89bd9fd6381f52bba547943c5e02b
Mayank Choudhary <7mayankchoudhary7@gmail.com>
2021-03-07 13:05:28 +0530

    Update urls.py

--- merchants/urls.py
+++ merchants/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
     path('merchant/login/', views.merchant_login, name="merchantLogin"),
     path('merchant/events/', views.merchant_events, name="merchantEvents"),
     path('merchant/welcome/', views.merchant_welcome, name="merchantWelcome")
-    path('merchant/spinform/', views.spin_event, name="merchantspin"),
-    path('merchant/qrform', views.qrcode_event, name="merchantqr"),
-    path('merchant/scrachform', views.scrach_event, name="merchantscrach")
 ]
